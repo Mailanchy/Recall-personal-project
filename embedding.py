13a7be76fc3401a7fa3f1931310cd0c6f91fe435
Husaima Mailanchy T K <husaimamailanchytk@Husaimas-MacBook-Air.local>
2026-09-21 16:48:33 +0530

    Day 1-2: Basic chunking with respect to headers done.

---
+++ embedding.py
@@ -0,0 +1,24 @@
+from langchain_text_splitters import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
+
+# Extract text from a file
+def extract_text(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+# Chunk the extracted text
+def chunk_text(text):
+    # Split the text based on Markdown headers
+    header_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=[('#','Header 1'),('##','Header 2'),('###','Header 3')],)
+    formatted_output = header_splitter.split_text(text)
+    print(formatted_output,'\n\n')
+    # Further split the text into smaller meaningful chunks. There is a chance that the text under each header is too long, so we will split it into smaller chunks.
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=9, chunk_overlap=5)
+    chunks = text_splitter.split_documents(formatted_output)
+
+    return chunks
+
+doc_text = extract_text("//Users/husaimamailanchytk/Documents/Mailu/Recall/notes.md")
+chunks = chunk_text(doc_text)
+print(chunks)
+
+
---
+++ notes.md
@@ -0,0 +1,10 @@
+# Hello
+contetn is hello
+## hello2
+content is hello 2
+### hello3
+content is hello 3
+# hi
+contetn is hi
+# check
+content is check
